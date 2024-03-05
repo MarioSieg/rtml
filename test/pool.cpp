68f4c81c5f6c8a10468870595b0e653203c4b469
Neo <mario.sieg.64@gmail.com>
2024-03-05 04:21:04 +0100

    Update pool.cpp

--- test/pool.cpp
+++ test/pool.cpp
@@ -38,3 +38,23 @@ TEST(pool, alloc_aligned) {
     *a = 0xdeadbeef;
     ASSERT_EQ(*a, 0xdeadbeef);
 }
+
+TEST(pool, alloc_type) {
+    static constinit bool called = false;
+    called = false;
+    struct alignas(64) test {
+        test() { called = true; }
+        const int vv {-128};
+    };
+    pool p {0xff};
+    ASSERT_EQ(p.num_allocs(), 0);
+    ASSERT_EQ(p.size(), 0xff);
+    ASSERT_EQ(p.bytes_allocated(), 0);
+    test* a {p.alloc<test>()};
+    ASSERT_TRUE(called);
+    ASSERT_EQ(a->vv, -128);
+    ASSERT_EQ(p.num_allocs(), 1);
+    ASSERT_EQ(p.size(), 0xff);
+    ASSERT_EQ(p.bytes_allocated(), sizeof(test)+alignof(test)-1);
+    ASSERT_EQ(std::bit_cast<std::uintptr_t>(a) % alignof(test), 0);
+}
