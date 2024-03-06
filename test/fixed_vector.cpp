729eb7280d826407125e3eee5fad340724791426
Neo <mario.sieg.64@gmail.com>
2024-03-06 11:58:43 +0100

    Update fixed_vector.cpp

--- test/fixed_vector.cpp
+++ test/fixed_vector.cpp
@@ -7,12 +7,16 @@
 using namespace rtml;
 
 TEST(fixed_vector, basic) {
-    fixed_vector<int, 4> vec;
+    fixed_vector<int, 4> vec {};
     static_assert(sizeof(vec) == sizeof(int) * 4 + sizeof(std::size_t));
+    ASSERT_TRUE(vec.empty());
+    ASSERT_FALSE(vec.full());
     vec.emplace_back(1);
     vec.emplace_back(2);
     vec.emplace_back(3);
     vec.emplace_back(4);
+    ASSERT_FALSE(vec.empty());
+    ASSERT_TRUE(vec.full());
     ASSERT_EQ(vec.size(), 4);
     ASSERT_EQ(vec[0], 1);
     ASSERT_EQ(vec[1], 2);
