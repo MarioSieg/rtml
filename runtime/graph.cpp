59960dd112f49452804b1b7d30dab1f79bef4589
Neo <mario.sieg.64@gmail.com>
2024-03-20 17:56:29 +0100

    Graph operations

--- runtime/graph.cpp
+++ runtime/graph.cpp
@@ -5,8 +5,6 @@
 #include "tensor.hpp"
 #include "blas.hpp"
 
-#if 0
-
 namespace rtml::graph {
     [[nodiscard]] static constexpr auto validate_unary_op(const tensor* const dst, const std::span<const tensor*> src) -> bool {
         const tensor* const r {dst};
@@ -156,5 +154,3 @@ namespace rtml::graph {
         callback(root);
     }
 }
-
-#endif
--- runtime/graph.hpp
+++ runtime/graph.hpp
@@ -2,23 +2,15 @@
 
 #pragma once
 
-#if 0
-
-#include <array>
-#include <cstdint>
-#include <string_view>
+#include <functional>
 #include <span>
 
-namespace rtml {
-    template <typename T>
-}
+#include "tensor_base.hpp"
 
 namespace rtml::graph {
     #define rtml_co ,
     /* Opcodes: Mnemonic, Operands, Info Mnemonic */
     #define rtml_opcode_def(_, __) \
-        /* Nullary ops */\
-        _(nop, 0, "nop")__\
         /* Unary ops */\
         _(softmax, 1, "softmax")__\
         _(sigmoid, 1, "sigmoid")__\
@@ -41,30 +33,146 @@ namespace rtml::graph {
     #undef _
 
     #define _(mnemonic, operands, name) ((operands)&0xff)
-    static constexpr std::array<std::uint32_t, $count> k_operands {
+    constexpr std::array<std::uint32_t, $count> k_operands {
         rtml_opcode_def(_, rtml_co)
     };
     #undef _
 
     #define _(mnemonic, operands, name) name
-    static constexpr std::array<std::string_view, $count> k_names {
+    constexpr std::array<std::string_view, $count> k_names {
         rtml_opcode_def(_, rtml_co)
     };
     #undef _
 
-    using validate_function = auto (const tensor* dst, std::span<const tensor*> src) -> bool;
-    using eval_function = auto (tensor* dst, std::span<const tensor*> src) noexcept -> void;
+    template <typename S> requires is_dtype<S>
+    using validate_function = auto (const tensor<S>* dst, std::span<const tensor<S>*> src) -> bool;
+    template <typename S> requires is_dtype<S>
+    using eval_function = auto (tensor<S>* dst, std::span<const tensor<S>*> src) noexcept -> void;
 
     enum class graph_eval_order : bool {
         left_to_right,
         right_to_left
     };
 
-    extern auto graph_visit(
-        const tensor* root,
-        graph_eval_order order,
-        const std::function<auto (const tensor* t) -> void>& callback
-    ) -> void;
-}
+    template <const graph_eval_order order, typename S, typename F, typename... Args>
+        requires is_dtype<S> && std::is_invocable_r_v<void, const tensor<S>*, Args...>
+    auto graph_visit(
+        const tensor<S>* root,
+        F&& callback,
+        Args&&... args
+    ) -> void {
+        const auto& operands {root->operands()};
+        for (std::size_t i {}; i < operands.size(); ++i) {
+            std::size_t ii;
+            if constexpr (order == graph_eval_order::left_to_right) ii = i;
+            else ii = operands.size() - i - 1;
+            graph_visit(operands[ii], order, std::forward<F>(callback), std::forward<Args>(args)...);
+        }
+        std::invoke(callback, root, std::forward<Args>(args)...);
+    }
 
-#endif
+    // all validation functions go here
+    namespace validators {
+        template <typename S> requires is_dtype<S>
+        [[nodiscard]] constexpr auto validate_unary_op(
+            const tensor<S>* const dst,
+            const std::span<const tensor<S>*> src
+        ) -> bool {
+            const tensor<S>* const r {dst};
+            if (!r) [[unlikely]] {
+                return false;
+            }
+            if (const auto num {static_cast<std::size_t>(r->opcode())}; k_operands[num] != src.size() && src.size() == 1) [[unlikely]] {
+                rtml_log_error("Operand count mismatch, expected {}, got {}", num, src.size());
+                return false;
+            }
+            const tensor<S>* const x {src[0]};
+            if (!x) [[unlikely]] {
+                return false;
+            }
+            if (!x->is_dense_except_dim1()) [[unlikely]] {
+                return false;
+            }
+            if (!r->is_dense_except_dim1()) [[unlikely]] {
+                return false;
+            }
+            if (r->is_shape_eq(x)) [[unlikely]] {
+                return false;
+            }
+            return true;
+        }
+
+        template <typename S> requires is_dtype<S>
+        [[nodiscard]] constexpr auto validate_binary_op(
+            const tensor<S>* const dst,
+            const std::span<const tensor<S>*> src
+        ) -> bool {
+            const tensor<S>* const r {dst};
+            if (!r) [[unlikely]] {
+                return false;
+            }
+            if (const auto num {static_cast<std::size_t>(r->opcode())}; k_operands[num] != src.size() && src.size() == 2) [[unlikely]] {
+                rtml_log_error("Operand count mismatch, expected {}, got {}", num, src.size());
+                return false;
+            }
+            const tensor<S>* const x {src[0]};
+            const tensor<S>* const y {src[1]};
+            if (!x || !y) [[unlikely]] {
+                return false;
+            }
+            if (x->strides()[0] != sizeof(float)) [[unlikely]] {
+                return false;
+            }
+            if (r->strides()[0] != sizeof(float)) [[unlikely]] {
+                return false;
+            }
+            if (!y->can_repeat(x)) [[unlikely]] {
+                return false;
+            }
+            if (!x->is_shape_eq(r)) [[unlikely]] {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    namespace evaluators {
+        template <typename S> requires is_dtype<S>
+        constexpr auto evaluate_unary_op(
+            const tensor<S>* const dst,
+            const std::span<const tensor<S>*> src
+        ) -> void {
+
+        }
+        template <typename S> requires is_dtype<S>
+        constexpr auto evaluate_binary_op(
+            const tensor<S>* const dst,
+            const std::span<const tensor<S>*> src
+        ) -> void {
+
+        }
+    }
+
+    template <typename S> requires is_dtype<S>;
+    struct routines;
+
+    template <>
+    struct routines final {
+        static constexpr std::array<validate_function<dtypes::f32>*, $count> validators {
+            [] consteval { // Autogenerate table of validation functions for unary and binary ops
+                std::array<validate_function<dtypes::f32>*, $count> result {};
+                for (std::size_t i {}; i < $count; ++i) {
+                    if (k_operands[i] == 1) { // unary op
+                        result[i] = &validators::validate_unary_op<dtypes::f32>;
+                    } else { // binary op
+                        result[i] = &validators::validate_binary_op<dtypes::f32>;
+                    }
+                }
+                return result;
+            }()
+        };
+        static constexpr std::array<eval_function<dtypes::f32>*, $count> evaluators {
+
+        };
+    };
+}
